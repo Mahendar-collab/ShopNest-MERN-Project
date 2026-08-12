9a32f68f0a4f396a34f2c3678b43d90bb02e8917
mahendar <mahendarn915@gmail.com>
2026-08-12 19:43:40 +0530

    Initial commit

---
+++ backend/controller/orderController.js
@@ -0,0 +1,70 @@
+const Order = require('../model/Order');
+
+const sendEmail = require('../utils/sendEmail');
+
+//Create a new order
+const createOrder = async (req,res)=>{
+    try{
+        const {items,totalAmount,address,paymentId} = req.body;
+        if(!items || items.length === 0 || !totalAmount || !address){
+            return res.status(400).json({message:'Invalid order data'});
+        }
+        else{
+            const order = new Order({
+                user:req.user._id,
+                items,
+                totalAmount,
+                address,
+                paymentId
+            });
+            await order.save();
+            const message = `Dear ${req.user.name},\\Thank you for your order! Your order has been successfully created with the following details.\n\norder ID:${order._id}\nTotal Amount:${totalAmount}\nshipping Address: ${address}\n\nWe will notify you once your order is shipped.\n\nBest regards.\nShopNest Team`;
+            await sendEmail(req.user.email,'Order created',message);
+            res.status(201).json({message:'Order created successfully',order});
+        }
+    }catch(error){
+        console.error("Create Order Error:", error);
+
+        res.status(500).json({
+            message: "Error creating order",
+            error: error.message
+        });
+    }
+}
+
+const myOrders = async (req,res)=>{
+    try {
+        const orders = await Order.find({user:req.user._id}).populate('items.productId','name price');
+        res.json(orders);
+    } catch (error) {
+        res.status(500).json({message:'Error fetching orders',error});
+    }
+}
+
+const getOrders = async (req,res)=>{
+    try {
+        const orders = await Order.find({}).populate('user','id name').populate('items.productId','name price');
+        res.json(orders);
+    } catch (error) {
+        res.status(500).json({message:'Error fetching orders',error});
+    }
+}
+
+const updateOrderStatus = async (req,res)=>{
+    try {
+        const { status } = req.body;
+        const order = await Order.findById(req.params.id);
+        if(order){
+            order.status = status;
+            await order.save();
+            res.json({message:'Order status updated', order});
+        }
+        else{
+            res.status(404).json({message:'Order not found'});
+        }
+    } catch (error) {
+        rse.status(500).json({message:'Error updating order status',error});
+    }
+}
+
+module.exports ={createOrder,myOrders,getOrders,updateOrderStatus};
